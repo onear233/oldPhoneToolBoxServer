b22efded9ea42c9f063a9324b39073e78c0484e3
microDreamSheep <atthemc313128460@gmail.com>
2022-08-21 13:59:57 +0800

    细节修改

--- src/main/java/live/midreamsheep/optb/data/SocketConfig.java
+++ src/main/java/live/midreamsheep/optb/data/SocketConfig.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class SocketConfig {
     public static String IP = "127.0.0.1";
-    public static int PORT = 6660;
+    public static int PORT = 7751;
     public static final int MONITOR_PORT= 13256;
     public static final int DOWNLOAD_PORT = 13760;
     public static File DOWNLOAD_FILE = new File("E://download");
